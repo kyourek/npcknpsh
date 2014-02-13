b92ae368a8390fa39e2d1f9606eaf7e512a3817c
Ken Yourek <ken@yourek.net>
2014-02-12 19:06:53 -0600

    Added pack or push option to cmd line args.

--- src/npcknpsh/build.py
+++ src/npcknpsh/build.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 
@@ -41,13 +42,29 @@ class Build:
 
             current_path = next_path
 
-    def process(self):
+    def process(self, pack_or_push):
 
         project_dir = self.project_dir()
         project_class = self.project_class()
 
         project = project_class(project_dir)
-        project.pack()
+
+        err = 0
+
+        if pack_or_push == 'pack':
+            err = project.pack()
+        if pack_or_push == 'push':
+            err = project.push()
+
+        return err
 
 if __name__ == '__main__':
-    Build().process()
+
+    parser = argparse.ArgumentParser(
+        description = 'Test, pack, and push NuGet packages for multiple framework version targets.'
+    )
+    parser.add_argument('pack_or_push', metavar = 'P', type = str, help = 'pack --or-- push')
+
+    args = parser.parse_args()
+
+    Build().process(args.pack_or_push)
